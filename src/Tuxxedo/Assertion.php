b8ada48e03899ce23e76242576933d5715d7ae65
Kalle Sommer Nielsen <kalle@php.net>
2020-10-24 23:58:44 +0300

    Added missing files

---
+++ src/Tuxxedo/Assertion.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ * Tuxxedo Engine
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ *
+ * @copyright 	2006-2020 Kalle Sommer Nielsen <kalle@tuxxedo.app>
+ * @license 	MIT
+ *
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ */
+
+declare(strict_types = 1);
+
+namespace Tuxxedo;
+
+class Assertion
+{
+	final private function __construct()
+	{
+	}
+
+	final private function __clone()
+	{
+	}
+
+	public function assert(bool $expression, string $message = null) : void
+	{
+		assert($expression, new AssertionException($message));
+	}
+}
---
+++ src/Tuxxedo/AssertionException.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ * Tuxxedo Engine
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ *
+ * @copyright 	2006-2020 Kalle Sommer Nielsen <kalle@tuxxedo.app>
+ * @license 	MIT
+ *
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ */
+
+declare(strict_types = 1);
+
+namespace Tuxxedo;
+
+class AssertionException extends Exception
+{
+	/**
+	 * @param string|null $message
+	 */
+	public function __construct(string $message = null)
+	{
+		if ($message !== null) {
+			parent::__construct(
+				'Assertion failure: %s',
+				$message
+			);
+		} else {
+			parent::__construct(
+				'Assertion failure'
+			);
+		}
+	}
+}

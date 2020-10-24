d9a9181610cf6fb803ce36d0cc82a614af90c173
Kalle Sommer Nielsen <kalle@php.net>
2020-10-24 23:52:20 +0300

    Exceptions and Assertions

--- src/Tuxxedo/Exception.php
+++ src/Tuxxedo/Exception.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ * Tuxxedo Engine
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ *
+ * @copyright 	2006-2020 Kalle Sommer Nielsen <kalle@tuxxedo.app>
+ * @license 	MIT
+ *
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ */
+
+declare(strict_types = 1);
+
+namespace Tuxxedo;
+
+class Exception extends \Exception
+{
+	/**
+	 * @param string $message
+	 * @param string ...
+	 */
+	public function __construct(string $message)
+	{
+		parent::__construct(
+			\call_user_func_array('\sprintf', \func_get_args())
+		);
+	}
+}

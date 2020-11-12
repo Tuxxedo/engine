4a417fca6a4a5ee95b9f54e9af0820f51f3cc095
Kalle Sommer Nielsen <kalle@php.net>
2020-11-12 06:44:49 +0200

    Application skeleton

---
+++ src/Tuxxedo/Application.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ * Tuxxedo Engine
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ *
+ * @copyright 	2006-2020 Kalle Sommer Nielsen <kalle@tuxxedo.app>
+ * @license 	MIT
+ *
+ * ^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+ */
+
+declare(strict_types = 1);
+
+namespace Tuxxedo;
+
+abstract class Application
+{
+	protected Di $di;
+
+	public function __construct(Di $di)
+	{
+		$this->di = $di;
+	}
+
+	abstract public function run() : void;
+}

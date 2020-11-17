192ec261b55f47ade36ff51701e05830503d6e01
DColt <github@dcolt.org>
2020-11-17 22:36:11 +0100

    Attempt to align match results

--- src/Tuxxedo/Database/Driver/Postgresql/Result.php
+++ src/Tuxxedo/Database/Driver/Postgresql/Result.php
@@ -160,10 +160,10 @@ class Result implements ResultInterface
 			$numberOfFields = \pg_num_fields($this->result);
 			for ($i = 0; $i < $numberOfFields; $i++) {
 				$fieldType = match(\pg_field_type($this->result, $i)) {
-					'bool' => 'bool',
-					'int2','int4','int8' => 'int',
-					'float4','float8' => 'float',
-					default => 'string',
+					'bool'			=> 'bool',
+					'int2','int4','int8'	=> 'int',
+					'float4','float8'	=> 'float',
+					default			=> 'string',
 				};
 				\settype($array[$i], $fieldType);
 			}
@@ -187,7 +187,7 @@ class Result implements ResultInterface
 			if (empty($this->typeMap)) {
 				$this->createTypeMap();
 			}
-			
+
 			foreach ($this->typeMap as $key => $type) {
 				\settype($assoc[$key], $type);
 			}
